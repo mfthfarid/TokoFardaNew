ba4511f82c64754d548a8fc2788b0daadfc3c647
mfthfarid <mfthfarid1826@gmail.com>
2023-11-02 13:41:54 +0700

    create transaski

---
+++ admin/transaksiJual.php
@@ -0,0 +1,11 @@
+<?php 
+    require "../koneksi.php";
+    
+    if (isset($_POST['submit'])) {
+        $nama_barang = $_POST['nama_barang'];
+        $jumlah = $_POST['jumlah'];
+        $harga = $_POST['harga'];
+
+        $total
+    }
+?>
