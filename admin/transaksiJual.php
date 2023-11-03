a8af9cdbd81ff75fd89eb5e37fe69ca5f04e841e
mfthfarid <mfthfarid1826@gmail.com>
2023-11-04 05:30:13 +0700

    create stock, create transaksi jual, create tambah barang, create func jual

--- admin/transaksiJual.php
+++ admin/transaksiJual.php
@@ -1,11 +1,7 @@
 <?php 
-    require "../koneksi.php";
-    
-    if (isset($_POST['submit'])) {
-        $nama_barang = $_POST['nama_barang'];
-        $jumlah = $_POST['jumlah'];
-        $harga = $_POST['harga'];
+    require '../koneksi.php';
+    require '../controller/jualController.php';
 
-        $total
-    }
+    $jualController = new transaksiJual();
+    $result = $jualController->showBarang();
 ?>
